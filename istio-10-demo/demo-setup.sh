2e4959b9da3c23fdfbb9442aa28d0f5b5bcb15c6
Lin Sun <lin.sun@solo.io>
2021-05-05 17:09:35 -0400

    update setup script

--- istio-10-demo/demo-setup.sh
+++ istio-10-demo/demo-setup.sh
@@ -3,25 +3,25 @@
 . $(dirname ${BASH_SOURCE})/../util.sh
 SOURCE_DIR=$PWD
 TAG="1.10.0-rc.0"
-desc "I have Istio 1.8.3 on my cluster"
-run "kubectl get pods -n istio-system"
+desc "I have Istio 1.8.3 and a few pods on my cluster"
 run "kubectl get services -n istio-system"
-
+run "kubectl get pods -A"
 
 desc "Let us download Istio $TAG"
 run "wget https://github.com/istio/istio/releases/download/1.10.0-rc.0/istio-$TAG-osx.tar.gz"
 run "tar -xvf istio-$TAG-osx.tar.gz"
+run "cd istio-$TAG/bin/"
 
 desc "Display istioctl version"
-run "istio-1.10.0-rc.0/bin/istioctl version"
+run "./istioctl version"
 
 desc "Check if it is same to install Istio 1.10"
-run "istio-1.10.0-rc.0/bin/istioctl x precheck"
-run "istio-1.10.0-rc.0/bin/istioctl analyze"
+run "./istioctl x precheck"
+run "./istioctl analyze"
 
 desc "Install Istiod 1.10"
-run "cat control-plane.yaml"
-run "istio-1.10.0-rc.0/bin/istioctl install -y -n istio-system --revision 1-10-0 -f control-plane.yaml"
+run "cat ../../control-plane.yaml"
+run "./istioctl install -y -n istio-system --revision 1-10-0 -f ../../control-plane.yaml"
 
 desc "Check Istiod $TAG pods"
 run "kubectl get pods -n istio-system"
@@ -31,4 +31,3 @@ run "kubectl exec -n istio-system -it deploy/istiod-1-10-0 -- pilot-discovery re
 
 desc "Check services to Istiod $TAG"
 run "kubectl exec -n istio-system -it deploy/istiod-1-10-0 -- pilot-discovery request GET /debug/registryz"
-
